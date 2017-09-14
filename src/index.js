08b58c6b346e4b9d81c6c3c83eb45106f36a6e45
Derik Hasvold <derikhasvold@parivedasolutions.com>
2017-09-13 17:04:45 -0700

    added eslint, removed references to app, hello world in index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Inspector extends React.Component {
+
+    render() {
+
+        return(
+            <h1>Hello World!</h1>
+        )
+    }
+}
+
+ReactDOM.render(<Inspector />, document.getElementById('root'));
 registerServiceWorker();
