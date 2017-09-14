1fcdeb82f8a1738f573f67bfbab57236e6d2bf34
Derik Hasvold <derikhasvold@parivedasolutions.com>
2017-09-14 15:47:55 -0700

    update search term now being called from SearchBar.js

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -21,7 +21,7 @@ class SearchBar extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-
+        this.props.updateSearchTerm(this.state.search)
     }
 
     render() {
