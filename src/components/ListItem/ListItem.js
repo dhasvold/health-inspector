7976171217057991d0663f12fd19be5125cf14f3
Derik Hasvold <derikhasvold@parivedasolutions.com>
2017-10-16 16:23:43 -0700

    meh

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,1 +1,8 @@
 import React from 'react'
+import Moment from 'moment'
+
+import GreenBadge from '../../assets/GreenBadge.png'
+import RedBadge from '../../assets/RedBadge.png'
+import YellowBadge from '../../assets/YellowBadge.png'
+import './ListItem.css'
+import NumbersLookup from '../../config/violationsLookup'
